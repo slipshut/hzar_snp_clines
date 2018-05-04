58df10810a105cdfa7077158515a9d2d0c44c856
Sara Lipshutz <sara.lipshutz@gmail.com>
2018-05-04 12:50:33 -0500

    Add names and site info, fixing to work with tables on cluster

--- Jac_SNP_cull_clinal_test.R
+++ Jac_SNP_cull_clinal_test.R
@@ -2,7 +2,8 @@
 source("HZAR_MLE.R")
 library(hzar)
 
-table_cull_hz_hwe <- read.csv("table_cull_hz_hwe_128000_test.csv")
+table_cull_hz_hwe <- read.csv("table_cull_hz_hwe_128000.csv",row.names=1)
+locality_info <- read.table("locality_info.txt",header=TRUE)
 str(table_cull_hz_hwe)
 
 use.allele="S1_991670203_T"
@@ -19,8 +20,8 @@ obs=hzar.doMolecularData1DPops(table_cull_hz_hwe$distance,
 models=list()
 models$free=hzar.makeCline1DFreq(data=obs,tails="none",scaling = "free")
 models$fixed=hzar.makeCline1DFreq(data=obs,tails="none",scaling = "fixed")
-
-MLE = lapply(models,cline.getMLE)
+fitR=lapply(models,hzar.first.fitRequest.old.ML,obsData=obs)
+MLE = lapply(fitR,cline.getMLE)
 
 null.AIC=hzar.AICc.hzar.dataGroup(hzar.dataGroup.null(obs))
 MLE.AIC=sapply(MLE,hzar.AICc.hzar.cline,nObs=sum(obs$frame$n))
