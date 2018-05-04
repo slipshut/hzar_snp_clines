267233f90706a136930ede06da5d0b14b769ef29
Sara Lipshutz <sara.lipshutz@gmail.com>
2018-05-04 12:24:35 -0500

    make function for evaluating null vs fixed and free models for diagnostic loci

--- Jac_SNP_cull_clinal_test.R
+++ Jac_SNP_cull_clinal_test.R
@@ -6,7 +6,10 @@ table_cull_hz_hwe <- read.csv("table_cull_hz_hwe_128000_test.csv")
 str(table_cull_hz_hwe)
 
 use.allele="S1_991670203_T"
-use.count=sub(pattern="[ACTG]$",replacement="N",x=use.allele)
+
+cull.allele <- function(use.allele,table_cull_hz_hwe) {
+  
+  use.count=sub(pattern="[ACTG]$",replacement="N",x=use.allele)
 
 obs=hzar.doMolecularData1DPops(table_cull_hz_hwe$distance,
                                pObs = table_cull_hz_hwe[[use.allele]],
@@ -18,3 +21,10 @@ models$free=hzar.makeCline1DFreq(data=obs,tails="none",scaling = "free")
 models$fixed=hzar.makeCline1DFreq(data=obs,tails="none",scaling = "fixed")
 
 MLE = lapply(models,cline.getMLE)
+
+null.AIC=hzar.AICc.hzar.dataGroup(hzar.dataGroup.null(obs))
+MLE.AIC=sapply(MLE,hzar.AICc.hzar.cline,nObs=sum(obs$frame$n))
+
+if (any(null.AIC > 2 + MLE.AIC)) return(use.allele)
+return(character())
+}
