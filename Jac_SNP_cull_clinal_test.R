633869bb735e3bdbcfe8384df168b2d5381307b9
Sara Lipshutz <sara.lipshutz@gmail.com>
2018-05-04 12:06:12 -0500

    simple MLE fitting for loci

--- Jac_SNP_cull_clinal_test.R
+++ Jac_SNP_cull_clinal_test.R
@@ -1,5 +1,20 @@
 # Jacana SNP Cull for Clinal Loci
 source("HZAR_MLE.R")
 library(hzar)
+
 table_cull_hz_hwe <- read.csv("table_cull_hz_hwe_128000_test.csv")
 str(table_cull_hz_hwe)
+
+use.allele="S1_991670203_T"
+use.count=sub(pattern="[ACTG]$",replacement="N",x=use.allele)
+
+obs=hzar.doMolecularData1DPops(table_cull_hz_hwe$distance,
+                               pObs = table_cull_hz_hwe[[use.allele]],
+                               nEff = table_cull_hz_hwe[[use.count]],
+                               siteID = table_cull_hz_hwe$locationID)
+
+models=list()
+models$free=hzar.makeCline1DFreq(data=obs,tails="none",scaling = "free")
+models$fixed=hzar.makeCline1DFreq(data=obs,tails="none",scaling = "fixed")
+
+MLE = lapply(models,cline.getMLE)
